54954322293f2f4de31162a253d64defa0289df6
dtoillion <toillion@gmail.com>
2019-03-01 16:03:39 +0100

    change color based on day and night cycle

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  Color dayColor = Color.red;
+  Color nightColor = Color.blue;
+  Renderer rend;
+
+  void Start()
+  {
+    rend = GetComponent<Renderer> ();
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.tag == "NightTime")
+      rend.material.color = nightColor;
+    if (other.gameObject.tag == "DayTime")
+      rend.material.color = dayColor;
+  }
+
+}
