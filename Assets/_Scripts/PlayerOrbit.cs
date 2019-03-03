efe2b5be4ab4e7734d1a407cd959c73fbf61845a
dtoillion <toillion@gmail.com>
2019-03-03 15:43:34 +0100

    audio pulse and contrtoller update

---
+++ Assets/_Scripts/PlayerOrbit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOrbit : MonoBehaviour
+{
+  public float Speed;
+
+  void FixedUpdate()
+  {
+    transform.Rotate(0f, Speed, 0f, Space.Self);
+  }
+
+  void Update()
+  {
+    if(Input.GetKeyDown("space"))
+    {
+      Speed = Speed * -1;
+    }
+  }
+}
