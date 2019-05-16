87d4f439886412a9c2e520e819d87f20f362f3b4
dtoillion <toillion@gmail.com>
2019-05-16 12:03:48 +0200

    cooldown for reversing and enemies spawn safe at first

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -6,18 +6,43 @@ public class Enemy : MonoBehaviour
 {
   public Color nightColor;
   public Color dayColor;
+  public Color spawnColorA;
+  public Color spawnColorB;
+  public bool Dangerous;
+  private Collider sC;
   Renderer rend;
 
   void Start()
   {
     rend = GetComponent<Renderer> ();
+    sC = GetComponent<Collider> ();
+    sC.enabled = false;
+    Dangerous = false;
+    StartCoroutine(SpawnSafe());
+  }
+
+  IEnumerator SpawnSafe()
+  {
+    rend.material.color = spawnColorA;
+    yield return new WaitForSeconds(0.3f);
+    rend.material.color = spawnColorB;
+    yield return new WaitForSeconds(0.3f);
+    rend.material.color = spawnColorA;
+    yield return new WaitForSeconds(0.3f);
+    rend.material.color = spawnColorB;
+    yield return new WaitForSeconds(0.3f);
+    rend.material.color = spawnColorA;
+    yield return new WaitForSeconds(0.3f);
+    Dangerous = true;
+    sC.enabled = true;
+    StopCoroutine(SpawnSafe());
   }
 
   void OnTriggerStay(Collider other)
   {
-    if (other.gameObject.tag == "NightTime")
+    if ((other.gameObject.tag == "NightTime") && (Dangerous))
       rend.material.color = nightColor;
-    if (other.gameObject.tag == "DayTime")
+    if ((other.gameObject.tag == "DayTime") && (Dangerous))
       rend.material.color = dayColor;
   }
 
